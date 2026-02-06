fad33b8cccea711cb87010775b2e5f6571aef10d
unknown <a99701841@gmail.com>
2026-02-06 10:39:41 +0900

    word-game-update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WordGame from './components/WordGame'
+
+function App() {
+
+  return (
+    <div>
+      <WordGame startWord='사과'/>
+    </div>
+  )
+}
+
+export default App
